b31c844c72f1e00a696bbd1654a4373f50d07501
Rahul Rawat <rahul.rawat@maropost.com>
2024-05-23 00:38:46 +0530

    install nvm

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.bashrc
+
+nvm install 20
+
+npm install -g next
+
 npm install pm2@latest -g
+
+cd /var/www/html 
 
 npm install
