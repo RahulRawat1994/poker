56b67684bb6fa316c052dbf67764649d307af84a
Rahul Rawat <rr904196@gmail.com>
2024-05-23 10:02:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-source ~/.bashrc
-
-nvm install 20
-
 npm install -g next
 
 npm install pm2@latest -g
